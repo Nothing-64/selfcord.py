064db4755736cd35273098985001c3837d01299c
Nothing-64 <nullexcption@gmail.com>
2025-02-03 23:08:43 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,32 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import re
 
-setup(
-    name='selfcord.py',
-    version='1.0.0',
-    description='An optimized version of discord.py 1.7.3 supporting API v9',
-    author='Nothing',
-    author_email='support@cuddly-team.kro.kr',
-    url='https://github.com/Nothing-64/selfcord.py',
-    packages=find_packages(),
-    install_requires=[
-        'aiohttp', 
-        'websockets'
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
-)
+
+def derive_version() -> str:
+    version = ''
+    with open('discord/__init__.py') as f:
+        version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+    if not version:
+        raise RuntimeError('version is not set')
+
+    if version.endswith(('a', 'b', 'rc')):
+        # append version identifier based on commit count
+        try:
+            import subprocess
+
+            p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = p.communicate()
+            if out:
+                version += out.decode('utf-8').strip()
+            p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = p.communicate()
+            if out:
+                version += '+g' + out.decode('utf-8').strip()
+        except Exception:
+            pass
+
+    return version
+
+
+setup(version=derive_version())
